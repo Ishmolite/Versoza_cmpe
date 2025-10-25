b6e521bd701262650187adb36917b8b76d803f81
Ishmolite <versoza.edison0515@gmail.com>
2025-10-25 13:22:37 +0800

    School notes for CMPE

--- myPackage/Testfile.py
+++ myPackage/Testfile.py
@@ -1,21 +1,28 @@
 from time import sleep
 
-print("Accessing Data File Access...")
+print("Accessing Data File access...")
 print('Request halted, Invalid Access. To proceed, fill in the necessary details.')
 
 accessName = input("Requesting Access Name : ")
 
 if accessName == "Ishmol":
-    input("Are you sure you are " + accessName + "? ")
+    doubleEntry = input("Are you sure you are " + accessName + "? ")
 else:
     print("Access Denied.")
+    print("Data File access request has been halted.")
+
+if doubleEntry == "I don't know, are you?":
+    print("Initializing...")
+    print("Decrypting...")
+    sleep(2)
+    print("Providing access...")
+    sleep(0.5)
+    print("Access granted.")
+    print("Welcome, Ishmol")
+else:
+    print("Access Denied.")
+    print("Data File access request has been halted.")
 
 
 
-print("Initializing...")
-print("Decrypting...")
-sleep(2)
-print("Providing access...")
-sleep(0.5)
-print("Access granted.")
-print("Welcome, Ishmol")
+
--- myPackage/mySecondPythonFile.py
+++ myPackage/mySecondPythonFile.py
@@ -1,5 +1,224 @@
+'''
+10/11/2025
+WEEK 5 LESSON
+'''
+
+# PART 1 ------------------------------
+
 strFullName = "Versoza Edison"
 intLength = len(strFullName) #len for length
 
 print(strFullName)
 print(intLength)
+print(strFullName[0])
+print(strFullName[6])
+print(strFullName[7])
+print(strFullName[8])
+print(strFullName[13])
+print(strFullName[intLength-1])
+
+mySubString = strFullName[2:5:2] #[start included, end excluded, step]
+print(mySubString)
+
+# PART 2 ---------------------------------------
+
+strFullName = "Versoza Edison"
+
+#String Method
+
+newValue = strFullName.upper()
+print(newValue)
+
+newValue = strFullName.lower()
+print(newValue)
+
+newValue = strFullName.title()
+print(newValue)
+
+newValue = strFullName.capitalize()
+print(newValue)
+
+newValue = strFullName.split(" ")
+print(newValue)
+
+newValue = strFullName.split("er")
+print(newValue)
+
+newValue = strFullName.replace("Versoza","Dororong")
+print(newValue)
+
+newValue = strFullName.replace("Versoza"," ")
+print(newValue)
+
+myLastName = "Versoza"
+myFirstName = "Edison"
+myMiddleName = "Bautista"
+
+myFullNameList = [myLastName, myFirstName, myMiddleName]
+print("Doro Lover".join([myLastName, myFirstName, myMiddleName]))
+print("Ishmolite".join(myFullNameList))
+
+newValue =strFullName.count("Versoza")
+print(newValue)
+
+newValue = strFullName.index("o")
+print(newValue)
+
+#PART 3 --------------------------------------------------------
+
+#STRING
+
+'''
+Numeric types:
+Integer -10, 0, 2324
+Float 25.2323
+Complex 25+47j (i ampere, j impendance -> resistance + reactance)
+'''
+import math
+
+intA = 25
+intB = 15
+intC = 5
+intD = 2
+intE = 2
+myComA = 25+5j
+myComB = 10-10j
+comProd = myComA * myComB
+print(comProd)
+#arithmetic operations
+intSum = intA + intB + intC
+print(intSum)
+intDiff = intA - intB
+print(intDiff)
+intProd = intA * intB
+print(intProd)
+intQu = intA / intB
+print(intQu)
+intResult = intD ** intE
+print(intResult)
+#PMDAS
+
+#cosine, sine, tangent
+intAngle = 90
+result = round(math.cos(math.radians(intAngle)), 1)
+print(result)
+result = round(math.sin(math.radians(intAngle)), 1)
+print(result)
+result = round(math.tan(math.radians(intAngle)), 1)
+print(result)
+
+intX = 5
+factResult = math.factorial(intX)
+print(factResult)
+
+'''
+Comparison Operator
+==, >, <, >=, <=, !=
+Boolean data types are 'True or False'
+  >> 'True' or 'False' = String
+  >>'1' or '2' = Integer
+'''
+
+intA = 5
+intB = 6
+
+isResult = intA == intB
+print(isResult)
+
+isResult = intA <= intB
+print(isResult)
+
+isResult = intA >= intB
+print(isResult)
+
+isResult = intA > intB
+print(isResult)
+
+isResult = intA < intB
+print(isResult)
+
+isResult = intA != intB
+print(isResult)
+
+#PART 4 -------------------------------------------
+
+#membership operator in, not in (CONTAINS)
+isResult = "e" in "Madagascar"
+print(isResult) #false
+isResult = "e" not in "Madagascar"
+print(isResult) #true
+myList = ["Dorothy", "Lappland", "A2", "Amiya", "Hong Lu"]
+isResult = "Ishmael" in myList
+print(isResult) #false
+myList = ["Dorothy", "Lappland", "A2", "Amiya", "Hong Lu"]
+isResult = "Lappland" in myList
+print(isResult) #true
+
+#identity operator is, is not (EXACT)
+isResult = "doro" is "dorothy"
+print(isResult) #false
+isResult = "doro" is not "dorothy"
+print(isResult) #true
+
+isMyResult = isResult is True
+print(isMyResult)
+
+#PART 5 ----------------------------------------------------
+
+from traceback import print_tb
+
+myInput = "1231231hello213123 2541wo2312rl2131d"
+
+myOutput = ""
+
+for char in myInput:
+    if not char.isnumeric():
+        myOutput = myOutput + char
+
+print(myOutput)
+
+#PART 6 ----------------------------------------------------
+
+'''
+LOGICAL OPERATORS
+Not, and, or -------- nand, nor, exor, exnor
+Logic circuits and switching theory
+'''
+
+isGroupPassed = False
+
+passingGrade = 85
+markGrade = 75
+jennyGrade = 95
+arthurGrade = 86
+
+isMarkPassed = markGrade >= passingGrade
+isJennyPassed = jennyGrade >= passingGrade
+isArthurPassed = arthurGrade >= passingGrade
+
+isGroupPassed = isMarkPassed and isJennyPassed and isArthurPassed
+print(isGroupPassed) #False, because one of them failed.
+
+isGroupPassed = isMarkPassed or isJennyPassed or isArthurPassed
+print(isGroupPassed) #True, because at least one of them passed.
+
+# PART 7 ------------------------------------------------------
+
+intA = 555
+intB = 4
+intC = 5
+
+isDivisible = False #Initial Value
+remainder = intA % intB #modulus or % returns the remainder
+print(remainder)
+if remainder == 0:
+    isDivisible = True
+    print(isDivisible) #False, remainder 3
+
+isDivisible = False #Initial Value
+remainder = intA % intC
+print(remainder)
+if remainder == 0:
+    isDivisible = True
+    print(isDivisible) #True, remainder 0
+
